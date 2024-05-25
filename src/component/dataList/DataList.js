72da59527edcd29793678c470d02b5514584a673
Sheikh-hadi <sheikhgee322@gmail.com>
2024-05-25 07:34:28 +0500

    ++

--- src/component/dataList/DataList.js
+++ src/component/dataList/DataList.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import '../../App.css'
+import React, { useState } from 'react';
+import '../../App.css';
 import { Row, Col, Table, List, Avatar } from 'antd';
 import tableColumns from '../../model/tableColums';
 import tableDataSource from '../../model/tableDatasource';
 import dataList from '../../model/ListData';
-import Header from '.././header/Header';
+import Header from '../header/Header';
 import Footer from '../Footer';
 
 const DataList = () => {
+    const [showtable, setShowtable] = useState(false);
+
+    const handleShowTable = () => {
+        setShowtable(true);
+    };
+
     const data = dataList;
     const dataSource = tableDataSource;
     const columns = tableColumns;
+
     return (
         <div style={{ height: '100vh' }}>
-            <Header/>
+            <Header />
             <Row style={{ height: '100%' }}>
                 <Col span={6} style={{ height: '100%' }}>
                     <div
@@ -22,15 +29,14 @@ const DataList = () => {
                             height: '100%',
                             overflow: 'auto',
                             padding: '0 16px',
-                            // border: '1px solid rgba(140, 140, 140, 0.35)',
-                            borderRadius:"8px",
+                            borderRadius: "8px",
                         }}
                     >
                         <List
                             itemLayout="horizontal"
                             dataSource={data}
                             renderItem={(item, index) => (
-                                <List.Item>
+                                <List.Item key={index} onClick={handleShowTable}>
                                     <List.Item.Meta
                                         avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
                                         title={item.title}
@@ -41,21 +47,22 @@ const DataList = () => {
                         />
                     </div>
                 </Col>
-                <Col span={18} style={{ height: '100%' }}>
-                    <Table
-                        pagination={false}
-                        dataSource={dataSource}
-                        columns={columns}
-                        bordered={true}
-                        scroll={{ x: true }}
-                        style={{ height: '100%' }}
-                    />
-                </Col>
+                {showtable && (
+                    <Col span={18} style={{ height: '100%' }}>
+                        <Table
+                            pagination={false}
+                            dataSource={dataSource}
+                            columns={columns}
+                            bordered={true}
+                            scroll={{ x: true }}
+                            style={{ height: '100%' }}
+                        />
+                    </Col>
+                )}
             </Row>
-            <Footer/>
+            <Footer />
         </div>
     );
-
-}
+};
 
 export default DataList;
